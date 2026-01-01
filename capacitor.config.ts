22a315f42e49e2c19b5f9e4d69fd8498444deb11
cliztech <ctcheekyprints@gmail.com>
2026-01-01 18:00:22 +1100

    gogogog

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'bmad.method',
+  appName: 'bmad-method',
+  webDir: 'build'
+};
+
+export default config;
